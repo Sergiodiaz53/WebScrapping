c59a269b091353579c3c4b98fb623bcd1d6fbd6c
Sergio Díaz del Pino <sergiodiazdp@gmail.com>
2017-07-11 09:12:34 +0200

    Memory management II

--- StartScrap-cleaned.py
+++ StartScrap-cleaned.py
@@ -7,85 +7,87 @@ import random
 import json
 import csv
 import time
+import gc
 
-# User information
-username = 'username'
-password = 'password'
-url = 'myurl'
+def main():
+	# User information
+	username = 'myusername'
+	password = 'mypassword'
+	url = 'myurl'
 
-# Create session and browser
-session = Session()
-browser = RoboBrowser(session=session,history=True)
-browser.open(url)
+	# Create session and browser
+	session = Session()
+	browser = RoboBrowser(session=session,history=False)
+	browser.open(url)
 
-#Log into server
-form = browser.get_form(action="myaction")
-form['authorization[name]'] = username
-form['authorization[password]'] = password
-browser.session.headers['Referer'] = url
-browser.submit_form(form)
+	#Log into server
+	form = browser.get_form(action="authorization")
+	form['authorization[name]'] = username
+	form['authorization[password]'] = password
+	browser.session.headers['Referer'] = url
+	browser.submit_form(form)
 
-#Prepare output documents
-output_json_file  = open('data.json', 'w')
-output_csv_file = open('data.csv', 'w')
-w = csv.writer(output_csv_file)
+	step = 0
+	start_time = time.time()
 
-step = 0
-start_time = time.time()
+	#Retrieve job info from X to Y (backwards)
+	for x in range(1615590, 5383, -1):
+		current_time = time.time()
+		print("Retrieving job: ", step, "\t Time spent: ", round(current_time-start_time,2),  " seconds")
 
-#Retrieve job info from X to Y (backwards)
-for x in range(1647550, 5383, -1):
-	current_time = time.time()
-	print("Retrieving job: ", step, "\t Time spent: ", round(current_time-start_time,2),  " seconds")
+		#Prepare job URL
+		url = myurl + str(x)
 
-	#Prepare job URL
-	url = "myjoburl" + str(x)
+		try:
+			#Open page, retrieve table with info and the submit script
+			browser = RoboBrowser(session=session,history=False)
+			browser.open(url)
+			data = str(browser.select('table'))
+			submit_script = browser.select('#job_text')[0].get_text(strip=True)
 
-	try:
-		#Open page, retrieve table with info and the submit script
-		browser.open(url)
-		data = str(browser.select('table'))
-		submit_script = browser.select('#job_text')[0].get_text(strip=True)
+			soup = BeautifulSoup(data, 'html5lib')
 
-		soup = BeautifulSoup(data, 'html5lib')
+			#Parse table information into Python dictionary
+			retrievedDict = dict()
+			for label in soup.select("tr"):
+				if(label.select("td > strong")[0]):
+					if(label.select("td")[1]):
+						retrievedDict[label.select("td > strong")[0].get_text(strip=True)] = label.select("td")[1].get_text(strip=True)
+					else:
+						retrievedDict[label.select("td > strong")[0].get_text(strip=True)] = "NULL"
 
-		#Parse table information into Python dictionary
-		retrievedDict = dict()
-		for label in soup.select("tr"):
-			if(label.select("td > strong")[0]):
-				if(label.select("td")[1]):
-					retrievedDict[label.select("td > strong")[0].get_text(strip=True)] = label.select("td")[1].get_text(strip=True)
-				else:
-					retrievedDict[label.select("td > strong")[0].get_text(strip=True)] = "NULL"
+			retrievedDict["submit_script"] = submit_script
 
-		retrievedDict["submit_script"] = submit_script
+			#Dump job info in Json and CSV files
+			with open('data.json', 'a') as output_json_file:
+				json.dump(retrievedDict, output_json_file, sort_keys=True, indent=4)
 
-		#Dump job info in Json and CSV files
-		json.dump(retrievedDict, output_json_file, sort_keys=True, indent=4)
+			with open('data.csv', 'a') as output_csv_file:
+				w = csv.writer(output_csv_file)
+				if (step == 0): 
+					w.writerow(retrievedDict.keys())
+				w.writerow(retrievedDict.values())
 
-		if (step == 0): 
-			w.writerow(retrievedDict.keys())
-			w.writerow(retrievedDict.values())
-		else:
-			w.writerow(retrievedDict.values())
-	except(IndexError):
-		print("ERROR: Index out of bounds exception (Missing job). Skipping.")
-	except Exception as e:
-		print("ERROR: There was a problem retriving a job.")
-		print("Unexpected error: ", e)
-		print("Skipping")
 
-	#Free memory
-	del retrievedDict
-	gc.collect()
+		except(IndexError):
+			print("ERROR: Index out of bounds exception (Missing job). Skipping.")
+		except Exception as e:
+			print("ERROR: There was a problem retriving a job.")
+			print("Unexpected error: ", e)
+			print("Skipping")
 
-	#Wait random secs for next job
-	sleep(round(random.uniform(0.6, 1.2), 2))
-	step += 1
+		
+		#Free memory
+		del browser
+		gc.collect()
 
-end_time = time.time()
+		#Wait random secs for next job
+		sleep(round(random.uniform(0.6, 1.2), 2))
+		step += 1
 
-print("Total time spent: ", round(end_time-start_time, 2), "with a total of", step, "jobs.")
+	end_time = time.time()
 
-output_json_file.close()
-output_csv_file.close()
+	print("Total time spent: ", round(end_time-start_time, 2), "with a total of", step, "jobs.")
+
+if __name__ == "__main__":
+	main()
